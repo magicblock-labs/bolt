da846293b596b5b4ce28da2e453615b8a76e413e
Gabriele Picco <piccogabriele@gmail.com>
2024-01-20 18:48:30 +0100

    :recycle: Code refactor

--- clients/bolt-sdk/lib/accounts/World.d.ts
+++ clients/bolt-sdk/lib/accounts/World.d.ts
@@ -3,37 +3,54 @@ import * as beet from "@metaplex-foundation/beet";
 import * as web3 from "@solana/web3.js";
 import * as beetSolana from "@metaplex-foundation/beet-solana";
 export interface WorldArgs {
-    id: beet.bignum;
-    entities: beet.bignum;
+  id: beet.bignum;
+  entities: beet.bignum;
 }
 export declare const worldDiscriminator: number[];
 export declare class World implements WorldArgs {
-    readonly id: beet.bignum;
-    readonly entities: beet.bignum;
-    private constructor();
-    static fromArgs(args: WorldArgs): World;
-    static fromAccountInfo(accountInfo: web3.AccountInfo<Buffer>, offset?: number): [World, number];
-    static fromAccountAddress(connection: web3.Connection, address: web3.PublicKey, commitmentOrConfig?: web3.Commitment | web3.GetAccountInfoConfig): Promise<World>;
-    static gpaBuilder(programId?: web3.PublicKey): beetSolana.GpaBuilder<{
-        accountDiscriminator: any;
-        id: any;
-        entities: any;
-    }>;
-    static deserialize(buf: Buffer, offset?: number): [World, number];
-    serialize(): [Buffer, number];
-    static get byteSize(): number;
-    static getMinimumBalanceForRentExemption(connection: web3.Connection, commitment?: web3.Commitment): Promise<number>;
-    static hasCorrectByteSize(buf: Buffer, offset?: number): boolean;
-    pretty(): {
-        id: number | {
-            toNumber: () => number;
+  readonly id: beet.bignum;
+  readonly entities: beet.bignum;
+  private constructor();
+  static fromArgs(args: WorldArgs): World;
+  static fromAccountInfo(
+    accountInfo: web3.AccountInfo<Buffer>,
+    offset?: number
+  ): [World, number];
+  static fromAccountAddress(
+    connection: web3.Connection,
+    address: web3.PublicKey,
+    commitmentOrConfig?: web3.Commitment | web3.GetAccountInfoConfig
+  ): Promise<World>;
+  static gpaBuilder(programId?: web3.PublicKey): beetSolana.GpaBuilder<{
+    accountDiscriminator: any;
+    id: any;
+    entities: any;
+  }>;
+  static deserialize(buf: Buffer, offset?: number): [World, number];
+  serialize(): [Buffer, number];
+  static get byteSize(): number;
+  static getMinimumBalanceForRentExemption(
+    connection: web3.Connection,
+    commitment?: web3.Commitment
+  ): Promise<number>;
+  static hasCorrectByteSize(buf: Buffer, offset?: number): boolean;
+  pretty(): {
+    id:
+      | number
+      | {
+          toNumber: () => number;
         };
-        entities: number | {
-            toNumber: () => number;
+    entities:
+      | number
+      | {
+          toNumber: () => number;
         };
-    };
+  };
 }
-export declare const worldBeet: beet.BeetStruct<World, WorldArgs & {
+export declare const worldBeet: beet.BeetStruct<
+  World,
+  WorldArgs & {
     accountDiscriminator: number[];
-}>;
+  }
+>;
 //# sourceMappingURL=World.d.ts.map
--- clients/bolt-sdk/lib/instructions/apply.d.ts
+++ clients/bolt-sdk/lib/instructions/apply.d.ts
@@ -1,17 +1,23 @@
 import * as beet from "@metaplex-foundation/beet";
 import * as web3 from "@solana/web3.js";
 export interface ApplyInstructionArgs {
-    args: Uint8Array;
+  args: Uint8Array;
 }
-export declare const applyStruct: beet.FixableBeetArgsStruct<ApplyInstructionArgs & {
+export declare const applyStruct: beet.FixableBeetArgsStruct<
+  ApplyInstructionArgs & {
     instructionDiscriminator: number[];
-}>;
+  }
+>;
 export interface ApplyInstructionAccounts {
-    componentProgram: web3.PublicKey;
-    boltSystem: web3.PublicKey;
-    boltComponent: web3.PublicKey;
-    anchorRemainingAccounts?: web3.AccountMeta[];
+  componentProgram: web3.PublicKey;
+  boltSystem: web3.PublicKey;
+  boltComponent: web3.PublicKey;
+  anchorRemainingAccounts?: web3.AccountMeta[];
 }
 export declare const applyInstructionDiscriminator: number[];
-export declare function createApplyInstruction(accounts: ApplyInstructionAccounts, args: ApplyInstructionArgs, programId?: web3.PublicKey): web3.TransactionInstruction;
+export declare function createApplyInstruction(
+  accounts: ApplyInstructionAccounts,
+  args: ApplyInstructionArgs,
+  programId?: web3.PublicKey
+): web3.TransactionInstruction;
 //# sourceMappingURL=apply.d.ts.map
