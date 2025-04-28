64e6f43114ec98c81452444fa7167bee7a7d59f6
Danilo Guanabara <danilo@magicblock.gg>
2025-04-28 15:21:24 -0300

    :bug: Fixing cargo publish --dry-run with unpublished dependencies

---
+++ scripts/test-publish.sh
@@ -0,0 +1,26 @@
+DRY_RUN="true"
+DRY_RUN_FLAG=""
+if [ "${DRY_RUN}" = "true" ]; then
+DRY_RUN_FLAG="--dry-run"
+fi
+
+if [ "${DRY_RUN}" = "true" ]; then
+NO_VERIFY_FLAG="--no-verify"
+fi      
+
+cargo +nightly publish -Zpackage-workspace $DRY_RUN_FLAG $NO_VERIFY_FLAG \
+    -p world \
+    -p bolt-cli \
+    -p bolt-lang \
+    -p bolt-utils \
+    -p bolt-system \
+    -p bolt-component \
+    -p bolt-attribute-bolt-arguments \
+    -p bolt-attribute-bolt-component \
+    -p bolt-attribute-bolt-component-deserialize \
+    -p bolt-attribute-bolt-component-id \
+    -p bolt-attribute-bolt-delegate \
+    -p bolt-attribute-bolt-extra-accounts \
+    -p bolt-attribute-bolt-program \
+    -p bolt-attribute-bolt-system \
+    -p bolt-attribute-bolt-system-input
