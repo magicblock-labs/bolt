a8270bf36dd460beb689ae95635a3c12549c7f0e
Danilo Guanabara <danilo@sensorial.systems>
2025-08-13 02:17:54 -0300

    :recycle: Updating C# client

--- clients/csharp/Solana.Unity.Bolt.Test/ECSTest.cs
+++ clients/csharp/Solana.Unity.Bolt.Test/ECSTest.cs
@@ -108,9 +108,11 @@ namespace ECSTest {
 
         public static async Task ApplySimpleMovementSystemUpOnEntity1(Framework framework) {
             var apply = new ApplyAccounts() {
+                CpiAuth = WorldProgram.CPI_AUTH_ADDRESS,
                 Authority = framework.Wallet.Account.PublicKey,
                 BoltSystem = framework.SystemSimpleMovement,
                 World = framework.WorldPda,
+                Buffer = WorldProgram.FindBufferPda(),
             };
             var instruction = WorldProgram.Apply(apply, Bolt.World.SerializeArgs(new { direction = "Up" }));
             instruction.Keys.Add(AccountMeta.ReadOnly(framework.ExampleComponentPosition, false));
