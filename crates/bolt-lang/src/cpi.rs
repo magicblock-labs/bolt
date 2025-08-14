6e842515fee0fda8062ed7027c1e45037138a608
Danilo Guanabara <danilo@sensorial.systems>
2025-08-14 16:58:34 -0300

    :rotating_light: Fixing linter warnings

--- crates/bolt-lang/src/cpi.rs
+++ crates/bolt-lang/src/cpi.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 use crate::BoltError;
 
 #[inline(always)]
-pub fn checker<'info>(cpi_auth: &AccountInfo<'info>) -> Result<()> {
+pub fn checker(cpi_auth: &AccountInfo<'_>) -> Result<()> {
     if !cpi_auth.is_signer || cpi_auth.key != crate::world::World::cpi_auth_address() {
         return Err(BoltError::InvalidCaller.into());
     }
